7dd70460085cfe18db80911d1480e2b04a73ba2a
Darcy <darcymccoy4@gmail.com>
2026-02-26 12:33:52 -0500

    Delete failing filler tests

--- app/src/test/java/com/example/soen345/MainActivityTest.java
+++ app/src/test/java/com/example/soen345/MainActivityTest.java
@@ -2,23 +2,8 @@ package com.example.soen345;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class MainActivityTest {
-
-    private MainActivity activity;
-
-    @BeforeEach
-    void setUp() {
-        activity = new MainActivity();
-    }
-
-    @Test
-    void addReturnsSumOfTwoNumbers() {
-        assertEquals(5, activity.add(2, 3));
-    }
 
 }
