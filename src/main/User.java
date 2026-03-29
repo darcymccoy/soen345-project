c629a36184294cba27f121af9ef871d680cd334b
Darcy <darcymccoy4@gmail.com>
2026-03-29 15:43:12 -0400

    Add user login

--- src/main/User.java
+++ src/main/User.java
@@ -1,26 +1,47 @@
 package main;
 
 public class User {
+    private String userId;
     private String email;
     private long phoneNumber;
+    private String password;
+    private String role;
+
+    public User(String email, long phoneNumber, String password, String role) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.role = "user";
+    }
 
     public User(String email) {
         this.email = email;
+        this.role = "user";
     }
 
-    public User(long phoneNumber){
+    public User(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+        this.role = "user";
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+    public String getEmail() { return email; }
+    public long getPhoneNumber() { return phoneNumber; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 
     public String toJson() {
-        return "{\"email\":\"" + email + "\",\"phoneNumber\":" + phoneNumber + "}";
+        return "{\"email\":\"" + (email != null ? email : "") + "\""
+             + ",\"phoneNumber\":" + phoneNumber
+             + ",\"password\":\"" + (password != null ? password : "") + "\""
+             + ",\"role\":\"" + (role != null ? role : "user") + "\"}";
     }
 }
